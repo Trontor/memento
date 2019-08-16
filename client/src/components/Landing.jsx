74cd02777d9a3f48eda3eb557aa3507bd5d2878a
Gigi <gigiscleung@gmail.com>
2019-08-16 21:51:59 +1000

    set up css grid + theme file

--- client/src/components/Landing.jsx
+++ client/src/components/Landing.jsx
@@ -1,11 +1,26 @@
 import React, { Component } from 'react'
+import styled from "styled-components"
+
+const LandingLayout = styled.div`
+  display: grid;
+  grid-template-columns: 100%;
+
+  @media screen and (min-width: ${props => props.theme.breakpoints.tabletLanscape}) {
+    grid-template-columns: 1fr 1fr;
+  }
+`;
 
 export class Landing extends Component {
   render() {
     return (
-      <div>
-        
-      </div>
+      <LandingLayout>
+        <div>
+          Memento
+        </div>
+        <div>
+          Signup
+        </div>
+      </LandingLayout>
     )
   }
 }
---
+++ client/src/theme.js
@@ -0,0 +1,13 @@
+const theme = {
+  breakpoints: {
+    mobile: '414px',
+    tabletPortrait: '768px',
+    tabletLanscape: '1024px',
+    desktop: '1280px'
+  },
+  palette: {
+    text: '#493D57'
+  }
+}
+
+export { theme };
