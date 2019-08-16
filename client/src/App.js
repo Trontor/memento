65a8ced0355f06db01b9bbc80f591821d1482966
Gigi <gigiscleung@gmail.com>
2019-08-16 19:25:15 +1000

    created browser routes for /login + /signup

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { withRouter, Link } from "react-router-dom";
+import Login from './components/Login';
+import Signup from './components/Signup';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Link to="/login">Login</Link>
+      <Link to="/signup">Signup</Link>
+      <Route path="/login" component={Login}/>
+      <Route path="/signup" component={Signup}/>
+    </Router>
   );
 }
 
