ca2a9eed489d93e37533e2e23f6745cf3c4d4fd0
Gigi <gigiscleung@gmail.com>
2019-09-08 15:02:40 +1000

    style radio buttons

--- client/src/components/UploadMemento/UploadMemento.jsx
+++ client/src/components/UploadMemento/UploadMemento.jsx
@@ -1,19 +1,49 @@
-import React from "react";
+import React, {useState} from "react";
 import { Header } from "ui/Typography";
 import { Container } from 'ui/Helpers';
 import { InstructionLabel, InputField, FormHelpText, FormSection } from 'ui/Forms';
 import { ButtonPrimary } from 'ui/Buttons';
 import { AlignRight } from 'ui/Helpers';
-import { RadialSelect } from './UploadMementoStyles'
-
+import { RadioOption, RadioButton, RadioButtonStyle, RadioLabel } from './UploadMementoStyles'
 
 export default function UploadMemento() {
+  const [selectMementoType, setSelectMementoType] = useState("");
+  const handleRadioChange = e => {
+    const value = e.target.value;
+    setSelectMementoType(value);
+  };
+
   return (
     <Container>
       <Header underline>Create a Memento</Header>
 
       <FormSection>
         <InstructionLabel>Is your memento an anniversary or special event?</InstructionLabel>
+        <RadioOption>
+          <RadioButton
+            type="radio"
+            value="event"
+            name="mementoType"
+            checked={selectMementoType === "event"}
+            onChange={e => handleRadioChange(e)}/>
+          <RadioButtonStyle/>
+          <RadioLabel>
+            Yes, it is an anniversary or special event.
+          </RadioLabel>
+        </RadioOption>
+
+        <RadioOption>
+          <RadioButton
+            type="radio"
+            value="object"
+            name="mementoType"
+            checked={selectMementoType === "object"}
+            onChange={e => handleRadioChange(e)}/>
+          <RadioButtonStyle/>
+          <RadioLabel>
+            No, it is a special object.
+          </RadioLabel>
+        </RadioOption>
       </FormSection>
 
       <FormSection>
--- client/src/components/UploadMemento/UploadMementoStyles.jsx
+++ client/src/components/UploadMemento/UploadMementoStyles.jsx
@@ -1,1 +1,85 @@
-import components from "styled-components";
+import styled from "styled-components";
+import { lighten } from "polished";
+import { center } from "ui/Helpers"
+
+export const RadioOption = styled.div`
+  position: relative;
+  margin-bottom: 12px;
+  /* border: 1px solid red; */
+  display: flex;
+  align-items: center;
+`;
+
+export const RadioButtonStyle = styled.span`
+  display: inline-block;
+  /* top: 50%;
+  left: 50%; */
+  height: 20px;
+  width: 20px;
+  border: 1px solid ${props => lighten(0.6, props.theme.palette.text)};
+  border-radius: 50%;
+  cursor: pointer;
+  ${props => props.theme.mixins.hoverFade};
+
+  &:after {
+    display: none;
+    content: '';
+    width: 150%;
+    height: 150%;
+    background-color: ${props => props.theme.palette.main};
+    border-radius: 50%;
+    opacity: 0.15;
+    position: relative;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    ${props => props.theme.mixins.hoverFade};
+
+  }
+`;
+
+export const RadioButton = styled.input`
+  position: absolute;
+  opacity: 0;
+  cursor: pointer;
+  z-index: 1;
+  width: 100%;
+  height: 100%;
+
+  &:checked + ${RadioButtonStyle} {
+    border-color: ${props => lighten(0.15, props.theme.palette.main)};
+
+    &::after {
+      content: "";
+      display: block;
+      border-radius: 50%;
+      background-color: ${props => props.theme.palette.main};
+      width: 50%;
+      height: 50%;
+      opacity: 1;
+    }
+  }
+
+  &:hover + ${RadioButtonStyle} {
+    border-color: ${props => lighten(0.15, props.theme.palette.main)};
+
+    &:after {
+      display: block;
+    }
+  }
+
+  &:active + ${RadioButtonStyle} {
+    &::after {
+      display: block;
+      background-color: ${props => props.theme.palette.main};
+      opacity: 0.35;
+    }
+  }
+`;
+
+export const RadioLabel = styled.label`
+  display: inline-block;
+  /* position: absolute; */
+  /* transform: translateY(25%); */
+  margin-left: 10px;
+`
