9a3f3f1708a8a0d285807a1c66e5874da6978173
Valerie <valerie.febriana@gmail.com>
2019-09-09 21:28:38 +1000

    select city handleChange

--- client/src/components/Settings/Settings.jsx
+++ client/src/components/Settings/Settings.jsx
@@ -1,4 +1,61 @@
 import React, { useState } from "react";
+import { Container } from "ui/Helpers";
+import { Header } from "ui/Typography";
+import { SettingsHeader, HeaderButton } from "./SettingsStyles";
+import SettingsProfile from "./SettingsProfile";
+import SettingsAccount from "./SettingsAccount";
+
+export default function Settings() {
+  const [settingsHeader, setSettingsHeader] = useState({
+    profile: true,
+    account: false
+  });
+
+  const settingsOpened = settingsName => {
+    if (
+      (settingsHeader.profile === true &&
+        settingsName.target.value === "profile") ||
+      (settingsHeader.account === true &&
+        settingsName.target.value === "account")
+    ) {
+      setSettingsHeader({
+        profile: settingsHeader.profile,
+        account: settingsHeader.account
+      });
+    } else {
+      setSettingsHeader({
+        profile: !settingsHeader.profile,
+        account: !settingsHeader.account
+      });
+    }
+  };
+
+  return (
+    <Container>
+      <Header underline>My Settings</Header>
+      <SettingsHeader>
+        <HeaderButton
+          value="profile"
+          onClick={settingsOpened}
+          menuClick={settingsHeader.profile}
+        >
+          Profile
+        </HeaderButton>
+        <HeaderButton
+          value="account"
+          onClick={settingsOpened}
+          menuClick={settingsHeader.account}
+        >
+          Account
+        </HeaderButton>
+      </SettingsHeader>
+      <SettingsProfile menuClick={settingsHeader} />
+      <SettingsAccount menuClick={settingsHeader} />
+    </Container>
+  );
+}
+
+/*import React, { useState } from "react";
 import { Formik } from "formik";
 import { Container } from "ui/Helpers";
 import { Header } from "ui/Typography";
@@ -80,4 +137,4 @@ export default function Settings() {
       )}
     />
   );
-}
+}*/
--- client/src/components/Settings/SettingsProfile.jsx
+++ client/src/components/Settings/SettingsProfile.jsx
@@ -1,20 +1,34 @@
 import React, { useState } from "react";
+import { Formik } from "formik";
 import {
   SettingsContainer,
   UploadPhoto,
-  UploadButton,
-  UploadLabel,
   Calendar,
   CountryPicker,
   CityPicker,
   PlaceWrapper,
   PlacesList
 } from "./SettingsStyles";
-import { FormSection, InputSection, InputLabel, InputField } from "ui/Forms";
+import { AddButton } from "ui/Buttons";
 import { DeleteButton } from "components/Invite/InviteStyles";
-import { ButtonPrimary, AddButton } from "ui/Buttons";
 
-export default function SettingsProfile({ menuClick, currentUser }) {
+import { FormSection, InputSection, InputLabel, InputField } from "ui/Forms";
+//import { useQuery } from "@apollo/react-hooks";
+//import GET_CURRENT_USER from "queries/GetCurrentUser";
+
+export default function SettingsProfile({ menuClick }) {
+  //const { data } = useQuery(GET_CURRENT_USER);
+
+  /*let defaultValues = {
+    firstName: "",
+    lastName: ""
+  };
+
+  if (data && data.currentUser) {
+    defaultValues.firstName = data.currentUser.firstName;
+    defaultValues.lastName = data.currentUser.lastName;
+  }*/
+
   const [birthday, setBirthday] = useState(null);
   const birthdayHandler = date => setBirthday(date);
   const genderList = ["Man", "Woman", "Other"];
@@ -24,17 +38,15 @@ export default function SettingsProfile({ menuClick, currentUser }) {
   const selectBirthCountry = value => setBirthCountry(value);
   const [birthCity, setBirthCity] = useState();
   const selectBirthCity = value => setBirthCity(value);
-  const [locationCountry, setLocationCountry] = useState();
-  const selectLocationCountry = value => setLocationCountry(value);
-  const [locationCity, setLocationCity] = useState();
-  const selectLocationCity = value => setLocationCity(value);
 
-  const [livePlaces, setLivePlaces] = useState([{ place: "", date: null }]);
+  const [livePlaces, setLivePlaces] = useState([{ city: "", date: null }]);
 
   function handleChange(index, event) {
     const places = [...livePlaces];
-    places[index].place = event.target.value;
+    places[index].city = event.target.value;
+    console.log(event.target.value, index);
     setLivePlaces(places);
+    console.log("after change", livePlaces);
   }
 
   const addPlace = place => {
@@ -48,121 +60,124 @@ export default function SettingsProfile({ menuClick, currentUser }) {
   };
 
   return (
-    <SettingsContainer menuClick={menuClick.profile}>
-      <FormSection>
-        <UploadPhoto type="file" id="file" />
-        <UploadButton size="35px" />
-        <UploadLabel htmlFor="file">Add a Profile Photo</UploadLabel>
-      </FormSection>
+    <Formik
+      initialValues={{ firstName: "Jane", lastName: "Doe" }}
+      render={props => (
+        <SettingsContainer menuClick={menuClick.profile}>
+          <FormSection>
+            <UploadPhoto type="file" id="file" />
+            <AddButton text="Add a Profile Photo">
+              <i className="fa fa-plus"></i>
+            </AddButton>
+          </FormSection>
 
-      <InputSection>
-        {/* First Name  */}
-        <InputLabel>First Name</InputLabel>
-        <InputField type="text" value={currentUser.firstName} />
-      </InputSection>
-
-      <InputSection>
-        <InputLabel>Last Name</InputLabel>
-        <InputField type="text" value={currentUser.lastName} />
-      </InputSection>
-
-      <InputSection>
-        <InputLabel>Birthday</InputLabel>
-        <Calendar
-          placeholderText="Click to select a date"
-          selected={birthday}
-          onChange={birthdayHandler}
-          showMonthDropdown
-          showYearDropdown
-          dropdownMode="select"
-          maxDate={new Date()}
-        />
-      </InputSection>
-
-      <FormSection>
-        <InputLabel>Gender</InputLabel>
-        {genderList.map(gender => (
           <InputSection>
-            <input
-              type="radio"
-              value={gender}
-              checked={genderOption === gender}
-              onChange={genderOptionHandler}
-            />
-            {gender}
-          </InputSection>
-        ))}
-      </FormSection>
-
-      <InputSection>
-        <InputLabel>Place of Birth</InputLabel>
-        <CountryPicker value={birthCountry} onChange={selectBirthCountry} />
-        <CityPicker
-          country={birthCountry}
-          value={birthCity}
-          onChange={selectBirthCity}
-        />
-      </InputSection>
-
-      <PlaceWrapper>
-        <InputLabel>Places You've Lived</InputLabel>
-        <InputLabel>Date Moved</InputLabel>
-      </PlaceWrapper>
-      <PlacesList>
-        {livePlaces.map((place, idx) => (
-          <PlaceWrapper>
+            {/* First Name  */}
+            <InputLabel>First Name</InputLabel>
             <InputField
               type="text"
-              placeholder="Select City"
-              style={{ width: "90%" }}
-              value={place.place}
-              onChange={e => handleChange(idx, e)}
+              name="firstName"
+              onChange={props.handleChange}
+              onBlur={props.handleBlur}
+              value={props.values.firstName}
             />
+          </InputSection>
 
+          <InputSection>
+            <InputLabel>Last Name</InputLabel>
+            <InputField
+              type="text"
+              name="lastName"
+              onChange={props.handleChange}
+              onBlur={props.handleBlur}
+              value={props.values.lastName}
+            />
+          </InputSection>
+
+          <InputSection>
+            <InputLabel>Birthday</InputLabel>
             <Calendar
               placeholderText="Click to select a date"
-              selected={place.date}
-              onChange={e => handleChange(idx, e)}
+              selected={birthday}
+              onChange={birthdayHandler}
               showMonthDropdown
               showYearDropdown
               dropdownMode="select"
               maxDate={new Date()}
             />
+          </InputSection>
 
-            {livePlaces.length > 1 && (
-              <DeleteButton onClick={() => deletePlace(idx)}>
-                <i class="fa fa-trash"></i>
-              </DeleteButton>
-            )}
+          <FormSection>
+            <InputLabel>Gender</InputLabel>
+            {genderList.map(gender => (
+              <InputSection>
+                <input
+                  type="radio"
+                  value={gender}
+                  checked={genderOption === gender}
+                  onChange={genderOptionHandler}
+                />
+                {gender}
+              </InputSection>
+            ))}
+          </FormSection>
+
+          <InputSection>
+            <InputLabel>Place of Birth</InputLabel>
+            <CountryPicker value={birthCountry} onChange={selectBirthCountry} />
+            <CityPicker
+              country={birthCountry}
+              value={birthCity}
+              onChange={selectBirthCity}
+            />
+          </InputSection>
+
+          <PlaceWrapper>
+            <InputLabel>Places You've Lived</InputLabel>
+            <InputLabel>Date Moved</InputLabel>
           </PlaceWrapper>
-        ))}
-      </PlacesList>
+          <PlacesList>
+            {livePlaces.map((place, idx) => (
+              <PlaceWrapper>
+                <InputField
+                  type="text"
+                  placeholder="Select City"
+                  style={{ width: "90%" }}
+                  value={place.city}
+                  onChange={e => handleChange(idx, e)}
+                />
+                {console.log("City:", place.city)}
 
-      {livePlaces.length < 10 && (
-        <AddButton text="Add another" onClick={() => addPlace({ place: "" })}>
-          <i class="fa fa-plus"></i>
-        </AddButton>
+                <Calendar
+                  placeholderText="Click to select a date"
+                  selected={place.date}
+                  onChange={e => handleChange(idx, e)}
+                  showMonthDropdown
+                  showYearDropdown
+                  dropdownMode="select"
+                  maxDate={new Date()}
+                />
+                {console.log("date:", place.date)}
+
+                {livePlaces.length > 1 && (
+                  <DeleteButton onClick={() => deletePlace(idx)}>
+                    <i className="fa fa-trash"></i>
+                  </DeleteButton>
+                )}
+              </PlaceWrapper>
+            ))}
+          </PlacesList>
+
+          {livePlaces.length < 10 && (
+            <AddButton
+              text="Add another"
+              onClick={() => addPlace({ city: "", date: null })}
+            >
+              <i className="fa fa-plus"></i>
+            </AddButton>
+          )}
+        </SettingsContainer>
       )}
-
-      {/*<InputSection>
-          <InputLabel>Places You've Lived</InputLabel>
-          <CountryPicker
-            value={locationCountry}
-            onChange={selectLocationCountry}
-          />
-          <CityPicker
-            country={locationCountry}
-            value={locationCity}
-            onChange={selectLocationCity}
-          />
-        </InputSection>
-        <InputSection>
-          <InputLabel>Date Moved</InputLabel>
-          <Calendar placeholderText="Click to select a date" />
-        </InputSection>*/}
-      <ButtonPrimary style={{ float: "right", margin: "10px" }}>
-        Save Changes
-      </ButtonPrimary>
-    </SettingsContainer>
+    />
   );
 }
