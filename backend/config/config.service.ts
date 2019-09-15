106c0df63596eb4b2ef7c18b23476e84b65dce3f
Trontor <rohyljoshi@hotmail.com>
2019-09-15 11:21:59 +1000

    fix: casted dotenv config output to EnvConfig interface

--- backend/config/config.service.ts
+++ backend/config/config.service.ts
@@ -31,7 +31,7 @@ export class ConfigService {
     } else {
       // Otherwise, just parse from .env
       console.log("Loading config from .env file");
-      config = dotenv.parse(".env");
+      config = dotenv.config() as EnvConfig;
       console.log("Config: ", config);
     }
     if (config) this.envConfig = this.validateInput(config);
