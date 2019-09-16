2ce3a9b181dd0de3320ca6cb461a419264c10268
giggleinu <gigiscleung@gmail.com>
2019-09-16 16:22:07 +1000

    Added tabs to FamilyGroup

--- client/src/components/FamilyGroup/FamilyGroup.jsx
+++ client/src/components/FamilyGroup/FamilyGroup.jsx
@@ -1,15 +1,22 @@
 import React, { useState } from "react";
 import { Container } from 'ui/Helpers';
-import { FamilyImg, FamilyHeader } from "./FamilyGroupStyles";
+import { FamilyImg, FamilyHeader, FamilyMenu, MenuTabs } from "./FamilyGroupStyles";
 
 export default function FamilyGroup() {
+
+  const menuTabs = ["Mementos", "Members", "Tags"]
 
   return(
    <>
      <FamilyImg/>
      <FamilyHeader>
-        <h2>Gigi's family</h2>
+        <h1>Gigi's family</h1>
       </FamilyHeader>
+      <FamilyMenu>
+        {menuTabs.map(tab => (
+          <MenuTabs>{tab}</MenuTabs>)
+        )}
+      </FamilyMenu>
      <Container noNav>
      </Container>
    </>
