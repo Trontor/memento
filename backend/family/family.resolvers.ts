10983936ca33acb80ac5bb67696e886a4193824d
Jackson Huang <jackson_huang@live.com>
2019-09-19 22:57:51 +1000

    Family update and create (#90)
    * create family: colour, image
    
    * * implemented updated family
    * prettier reformat

--- backend/family/family.resolvers.ts
+++ backend/family/family.resolvers.ts
@@ -1,81 +1,104 @@
 import {
-  Resolver,
-  Mutation,
-  Args,
-  ResolveProperty,
-  Parent,
-  Query
+    Resolver,
+    Mutation,
+    Args,
+    ResolveProperty,
+    Parent,
+    Query,
 } from "@nestjs/graphql";
 import { FamilyService } from "./family.service";
 import { UseGuards, Inject, forwardRef } from "@nestjs/common";
 import { JwtAuthGuard } from "../auth/guards/jwt-auth.guard";
 import { CurrentUser } from "../auth/decorators/current-user.decorator";
 import { Family } from "./dto/family.dto";
-import { CreateFamilyInput, JoinFamilyInput } from "./inputs/family.inputs";
+import {
+    CreateFamilyInput,
+    JoinFamilyInput,
+    UpdateFamilyInput,
+} from "./inputs/family.inputs";
 import { User } from "../user/dto/user.dto";
 import { UserService } from "../user/user.service";
 import { mapDocumentToFamilyDTO } from "./schema/family.mapper";
 import { FamilyDocument } from "./schema/family.schema";
 import { ID } from "type-graphql";
+import { FamilyAdminGuard } from "../auth/guards/family-admin.guard";
 
 /**
  * Resolves GraphQL mutations and queries related to families.
  */
 @Resolver(Family)
 export class FamilyResolver {
-  constructor(
-    private readonly familyService: FamilyService,
-    @Inject(forwardRef(() => UserService))
-    private readonly userService: UserService
-  ) {}
+    constructor(
+        private readonly familyService: FamilyService,
+        @Inject(forwardRef(() => UserService))
+        private readonly userService: UserService,
+    ) {}
 
-  /**
-   * Fetches a family's data.
-   */
-  @Query(returns => Family, { name: "family" })
-  async getFamily(
-    @Args({ name: "familyId", type: () => ID }) familyId: string
-  ) {
-    const doc: FamilyDocument = await this.familyService.getFamily(familyId);
-    return mapDocumentToFamilyDTO(doc);
-  }
+    /**
+     * Fetches a family's data.
+     */
+    @Query(returns => Family, { name: "family" })
+    async getFamily(
+        @Args({ name: "familyId", type: () => ID }) familyId: string,
+    ) {
+        const doc: FamilyDocument = await this.familyService.getFamily(
+            familyId,
+        );
+        return mapDocumentToFamilyDTO(doc);
+    }
 
-  /**
-   * Creates a new family.
-   * User must be authenticated via valid JWT.
-   */
-  @Mutation(returns => Family)
-  @UseGuards(JwtAuthGuard)
-  async createFamily(
-    @CurrentUser() currentUser: User,
-    @Args("input") input: CreateFamilyInput
-  ) {
-    const family = this.familyService.createFamily(currentUser, input);
-    return family;
-  }
+    /**
+     * Creates a new family.
+     * User must be authenticated via valid JWT.
+     */
+    @Mutation(returns => Family)
+    @UseGuards(JwtAuthGuard)
+    async createFamily(
+        @CurrentUser() currentUser: User,
+        @Args("input") input: CreateFamilyInput,
+    ) {
+        const family = this.familyService.createFamily(currentUser, input);
+        return family;
+    }
 
-  /**
-   * Resolves the `members` property on the `Family` object.
-   */
-  @ResolveProperty("members", returns => [User])
-  async getUsers(@Parent() { familyId }: Family) {
-    const { memberIds }: FamilyDocument = await this.familyService.getFamily(
-      familyId
-    );
-    if (!memberIds || memberIds.length === 0) return [];
-    const members: User[] = await this.userService.getUsers(memberIds);
-    return members;
-  }
+    /**
+     * Updates an existing family's details.
+     * User must be authenticated via valid JWT and be an admin of the family.
+     *
+     * @param user the current user who is making the update request
+     * @param input update family input fields
+     */
+    @Mutation(returns => Family)
+    @UseGuards(JwtAuthGuard, FamilyAdminGuard)
+    async updateFamily(
+        @CurrentUser() user: User,
+        @Args("input") input: UpdateFamilyInput,
+    ) {
+        return this.familyService.updateFamily(user, input);
+    }
 
-  /**
-   * Allows user to join a family.
-   */
-  @Mutation(returns => Family)
-  @UseGuards(JwtAuthGuard)
-  async joinFamily(
-    @CurrentUser() currentUser: User,
-    @Args("input") input: JoinFamilyInput
-  ) {
-    return await this.familyService.joinFamily(currentUser, input.inviteId);
-  }
+    /**
+     * Resolves the `members` property on the `Family` object.
+     */
+    @ResolveProperty("members", returns => [User])
+    async getUsers(@Parent() { familyId }: Family) {
+        const {
+            memberIds,
+        }: FamilyDocument = await this.familyService.getFamily(familyId);
+        if (!memberIds || memberIds.length === 0) return [];
+        const members: User[] = await this.userService.getUsers(memberIds);
+        return members;
+    }
+
+    /**
+     * Allows user to join a family.
+     */
+    @Mutation(returns => Family)
+    @UseGuards(JwtAuthGuard)
+    async joinFamily(
+        @CurrentUser() currentUser: User,
+        @Args("input") input: JoinFamilyInput,
+    ) {
+        return await this.familyService.joinFamily(currentUser, input.inviteId);
+    }
 }
--- backend/user/user.service.ts
+++ backend/user/user.service.ts
@@ -1,9 +1,10 @@
 import {
-  Injectable,
-  BadRequestException,
-  InternalServerErrorException,
-  UnauthorizedException,
-  Logger
+    Injectable,
+    BadRequestException,
+    InternalServerErrorException,
+    UnauthorizedException,
+    Logger,
+    ForbiddenException,
 } from "@nestjs/common";
 import { UserSignupInput, UpdateUserInput } from "./input/user.input";
 import { InjectModel } from "@nestjs/mongoose";
@@ -24,213 +25,215 @@ import { IUpdateUserData } from "./interfaces/user.update.interface";
  */
 @Injectable()
 export class UserService implements IUserService {
-  private readonly logger = new Logger(UserService.name);
-  constructor(
-    @InjectModel("User")
-    private readonly UserModel: Model<UserDocument>,
-    private readonly fileService: FileService
-  ) {}
-
-  /**
-   * Fetches and returns a `UserDocument` by `email`.
-   * @param email email of user to be found
-   */
-  async findOneByEmail(email: string): Promise<UserDocument> {
-    const user = await this.UserModel.findOne({
-      lowercaseEmail: email.toLowerCase()
-    }).exec();
-    if (!user) throw new UserNotFoundException();
-    return user;
-  }
-
-  /**
-   * Creates a new `UserDocument` for a newly registered user.
-   * @param input signup fields
-   */
-  async createUser(input: UserSignupInput): Promise<User> {
-    const createdUser = new this.UserModel(input);
-
-    let userDoc: UserDocument;
-    try {
-      userDoc = await createdUser.save();
-    } catch (err) {
-      this.logger.error(err);
-      throw this.evaluateMongoError(err, input);
-    }
-    return mapDocumentToUserDTO(userDoc);
-  }
-
-  /**
-   * Finds user by id.
-   * @param userId user id as a string
-   */
-  async findOneById(userId: string): Promise<User> {
-    const id = fromHexStringToObjectId(userId);
-    const user = await this.UserModel.findById(id);
-    if (!user) {
-      Logger.error(`user ${id} not found`);
-      throw new UserNotFoundException();
-    }
-    return mapDocumentToUserDTO(user);
-  }
-
-  /**
-   * Finds users by ids.
-   * @param userId user id as a string
-   */
-  async getUsers(userIds: string[]): Promise<User[]> {
-    if (userIds.length === 0) return [];
-    // more efficient to retrieve all docs in one round trip
-    const docs = await this.UserModel.find({
-      _id: {
-        $in: userIds.map(id => fromHexStringToObjectId(id))
-      }
-    });
-    return docs.map(doc => mapDocumentToUserDTO(doc));
-  }
-
-  /**
-   * Updates fields of a user.
-   * @param currentUser the user object attached to the GraphQL request
-   * @param fields updatable fields
-   */
-  async update(currentUser: User, fields: UpdateUserInput) {
-    const { userId: _, ...data } = fields;
-
-    // check if fields contain actual data
-    const hasData: boolean = Object.keys(data).length > 0;
-    if (!hasData) {
-      throw new BadRequestException("No data provided in UpdateUserInput");
-    }
-    this.logger.log(
-      `User ${currentUser.userId} updating ${JSON.stringify(fields)}`
-    );
-
-    if (!currentUser.userId)
-      throw new InternalServerErrorException("Current user not defined");
-
-    // authorization: users can only update their own profile details
-    if (currentUser.userId !== fields.userId) {
-      this.logger.log(
-        `current user id ${currentUser.userId} does not match updatee id ${fields.userId}`
-      );
-      throw new UnauthorizedException();
-    }
-
-    // upload image if provided
-    let imageUrl: string | undefined = undefined;
-    if (data.image) {
-      imageUrl = await this.fileService.uploadImage(data.image);
-    }
-
-    // extract image stream from `data` and replace with imageUrl
-    const { image, ...updatableData } = data;
-    const updateData: IUpdateUserData = { ...updatableData };
-    if (imageUrl) {
-      updateData.imageUrl = imageUrl;
-    }
-
-    // do update operation
-    const updatedUser = await this.UserModel.findOneAndUpdate(
-      { lowercaseEmail: currentUser.email.toLowerCase() }, // find by email
-      { ...updateData, imageUrl },
-      { new: true, runValidators: true }
-    );
-    if (!updatedUser)
-      throw new InternalServerErrorException("Could not update user");
-    const userDTO: User = mapDocumentToUserDTO(updatedUser);
-    this.logger.debug(userDTO);
-    return userDTO;
-  }
-
-  /**
-   * Updates role of a user in a family.
-   *
-   * @param updateeId id of user that is being updated
-   * @param input role data will be updated to this value
-   */
-  async updateRole(updateeId: string, input: RoleInput): Promise<User> {
-    const user = await this.UserModel.findOneAndUpdate(
-      {
-        _id: fromHexStringToObjectId(updateeId),
-        "roles.familyId": input.familyId
-      },
-      {
-        $set: {
-          "roles.$.familyRole": input.familyRole
-        }
-      },
-      {
-        new: true
-      }
-    );
-    if (!user) {
-      this.logger.error("User not in family");
-      throw new BadRequestException("Updatee not in family");
-    }
-    return mapDocumentToUserDTO(user);
-  }
-
-  /**
-   * Assigns new family role for a user joining a new family.
-   * @param userId user id
-   * @param input role input
-   */
-  async createRole(userId: string, input: RoleInput): Promise<UserDocument> {
-    this.logger.log(`User ${userId} creating role ${input}`);
-    const id: Types.ObjectId = fromHexStringToObjectId(userId);
-    this.logger.debug(`user id ${id}`);
+    private readonly logger = new Logger(UserService.name);
+    constructor(
+        @InjectModel("User")
+        private readonly UserModel: Model<UserDocument>,
+        private readonly fileService: FileService,
+    ) {}
 
     /**
-     * Inserts a new role object in the `roles` array if the
-     * the array does not contain an object with the same `familyId`.
+     * Fetches and returns a `UserDocument` by `email`.
+     * @param email email of user to be found
      */
-    const user = await this.UserModel.findOneAndUpdate(
-      {
-        _id: id,
-        // ensure familyId is unique in the `roles` array
-        "roles.familyId": { $ne: input.familyId }
-      },
-      {
-        $push: {
-          roles: {
-            familyId: input.familyId,
-            familyRole: input.familyRole
-          }
-        }
-      },
-      { new: true }
-    );
-    if (!user) throw new CreateRoleException();
-    this.logger.debug(`Updated user roles array: ${user.roles}`);
-    return user;
-  }
-
-  /**
-   * Reads a mongo database error and attempts to provide a better error message. If
-   * it is unable to produce a better error message, returns the original error message.
-   *
-   * @private
-   * @param {MongoError} error
-   * @param {UserSignupInput} UserSignupInput
-   * @returns {Error}
-   * @memberof UserService
-   */
-  private evaluateMongoError(
-    error: MongoError,
-    createUserInput: UserSignupInput
-  ): Error {
-    if (error.code === 11000) {
-      if (
-        error.message
-          .toLowerCase()
-          .includes(createUserInput.email.toLowerCase())
-      ) {
-        throw new BadRequestException(
-          `email ${createUserInput.email} is already registered`
-        );
-      }
+    async findOneByEmail(email: string): Promise<UserDocument> {
+        const user = await this.UserModel.findOne({
+            lowercaseEmail: email.toLowerCase(),
+        }).exec();
+        if (!user) throw new UserNotFoundException();
+        return user;
     }
-    throw new InternalServerErrorException(error.message);
-  }
+
+    /**
+     * Creates a new `UserDocument` for a newly registered user.
+     * @param input signup fields
+     */
+    async createUser(input: UserSignupInput): Promise<User> {
+        const createdUser = new this.UserModel(input);
+
+        let userDoc: UserDocument;
+        try {
+            userDoc = await createdUser.save();
+        } catch (err) {
+            this.logger.error(err);
+            throw this.evaluateMongoError(err, input);
+        }
+        return mapDocumentToUserDTO(userDoc);
+    }
+
+    /**
+     * Finds user by id.
+     * @param userId user id as a string
+     */
+    async findOneById(userId: string): Promise<User> {
+        const id = fromHexStringToObjectId(userId);
+        const user = await this.UserModel.findById(id);
+        if (!user) {
+            Logger.error(`user ${id} not found`);
+            throw new UserNotFoundException();
+        }
+        return mapDocumentToUserDTO(user);
+    }
+
+    /**
+     * Finds users by ids.
+     * @param userId user id as a string
+     */
+    async getUsers(userIds: string[]): Promise<User[]> {
+        if (userIds.length === 0) return [];
+        // more efficient to retrieve all docs in one round trip
+        const docs = await this.UserModel.find({
+            _id: {
+                $in: userIds.map(id => fromHexStringToObjectId(id)),
+            },
+        });
+        return docs.map(doc => mapDocumentToUserDTO(doc));
+    }
+
+    /**
+     * Updates fields of a user.
+     * @param currentUser the user object attached to the GraphQL request
+     * @param fields updatable fields
+     */
+    async update(currentUser: User, fields: UpdateUserInput) {
+        const { userId: _, ...data } = fields;
+
+        // check if fields contain actual data
+        const hasData: boolean = Object.keys(data).length > 0;
+        if (!hasData) {
+            throw new BadRequestException(
+                "No data provided in UpdateUserInput",
+            );
+        }
+        this.logger.log(
+            `User ${currentUser.userId} updating ${JSON.stringify(fields)}`,
+        );
+
+        if (!currentUser.userId)
+            throw new InternalServerErrorException("Current user not defined");
+
+        // authorization: users can only update their own profile details
+        if (currentUser.userId !== fields.userId) {
+            this.logger.log(
+                `current user id ${currentUser.userId} does not match updatee id ${fields.userId}`,
+            );
+            throw new ForbiddenException();
+        }
+
+        // upload image if provided
+        let imageUrl: string | undefined = undefined;
+        if (data.image) {
+            imageUrl = await this.fileService.uploadImage(data.image);
+        }
+
+        // extract image stream from `data` and replace with imageUrl
+        const { image, ...updatableData } = data;
+        const updateData: IUpdateUserData = { ...updatableData };
+        if (imageUrl) {
+            updateData.imageUrl = imageUrl;
+        }
+
+        // do update operation
+        const updatedUser = await this.UserModel.findOneAndUpdate(
+            { lowercaseEmail: currentUser.email.toLowerCase() }, // find by email
+            { ...updateData, imageUrl },
+            { new: true, runValidators: true },
+        );
+        if (!updatedUser)
+            throw new InternalServerErrorException("Could not update user");
+        const userDTO: User = mapDocumentToUserDTO(updatedUser);
+        this.logger.debug(userDTO);
+        return userDTO;
+    }
+
+    /**
+     * Updates role of a user in a family.
+     *
+     * @param updateeId id of user that is being updated
+     * @param input role data will be updated to this value
+     */
+    async updateRole(updateeId: string, input: RoleInput): Promise<User> {
+        const user = await this.UserModel.findOneAndUpdate(
+            {
+                _id: fromHexStringToObjectId(updateeId),
+                "roles.familyId": input.familyId,
+            },
+            {
+                $set: {
+                    "roles.$.familyRole": input.familyRole,
+                },
+            },
+            {
+                new: true,
+            },
+        );
+        if (!user) {
+            this.logger.error("User not in family");
+            throw new BadRequestException("Updatee not in family");
+        }
+        return mapDocumentToUserDTO(user);
+    }
+
+    /**
+     * Assigns new family role for a user joining a new family.
+     * @param userId user id
+     * @param input role input
+     */
+    async createRole(userId: string, input: RoleInput): Promise<UserDocument> {
+        this.logger.log(`User ${userId} creating role ${input}`);
+        const id: Types.ObjectId = fromHexStringToObjectId(userId);
+        this.logger.debug(`user id ${id}`);
+
+        /**
+         * Inserts a new role object in the `roles` array if the
+         * the array does not contain an object with the same `familyId`.
+         */
+        const user = await this.UserModel.findOneAndUpdate(
+            {
+                _id: id,
+                // ensure familyId is unique in the `roles` array
+                "roles.familyId": { $ne: input.familyId },
+            },
+            {
+                $push: {
+                    roles: {
+                        familyId: input.familyId,
+                        familyRole: input.familyRole,
+                    },
+                },
+            },
+            { new: true },
+        );
+        if (!user) throw new CreateRoleException();
+        this.logger.debug(`Updated user roles array: ${user.roles}`);
+        return user;
+    }
+
+    /**
+     * Reads a mongo database error and attempts to provide a better error message. If
+     * it is unable to produce a better error message, returns the original error message.
+     *
+     * @private
+     * @param {MongoError} error
+     * @param {UserSignupInput} UserSignupInput
+     * @returns {Error}
+     * @memberof UserService
+     */
+    private evaluateMongoError(
+        error: MongoError,
+        createUserInput: UserSignupInput,
+    ): Error {
+        if (error.code === 11000) {
+            if (
+                error.message
+                    .toLowerCase()
+                    .includes(createUserInput.email.toLowerCase())
+            ) {
+                throw new BadRequestException(
+                    `email ${createUserInput.email} is already registered`,
+                );
+            }
+        }
+        throw new InternalServerErrorException(error.message);
+    }
 }
