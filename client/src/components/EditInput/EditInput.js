a6d194101e0bd7b061ba8d26d96f1c26824f196a
giggleinu <gigiscleung@gmail.com>
2019-09-22 00:53:44 +1000

    add update button

---
+++ client/src/components/EditInput/EditInput.js
@@ -0,0 +1,9 @@
+import { ButtonSecondary } from "ui/Buttons";
+import { lighten } from "polished";
+import styled from "styled-components";
+
+export const UpdateButton = styled(ButtonSecondary)`
+  padding: 8px 12px;
+  margin-top: 8px;
+  font-size: 14px;
+`
