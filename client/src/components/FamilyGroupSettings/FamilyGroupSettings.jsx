820b03ed153153fedef803afc1ebb4bdbc3d84fe
giggleinu <gigiscleung@gmail.com>
2019-09-22 00:42:17 +1000

    Remove old input field

--- client/src/components/FamilyGroupSettings/FamilyGroupSettings.jsx
+++ client/src/components/FamilyGroupSettings/FamilyGroupSettings.jsx
@@ -20,7 +20,6 @@ import { useQuery } from "@apollo/react-hooks";
 
 export default function FamilyGroupSettings(props) {
   const familyId = props.match.params.id;
-  const [toggleEdit, setToggleEdit] = useState(false);
   const { data, loading, error } = useQuery(LOAD_FAMILY, {
     variables: { id: familyId },
   });
@@ -37,13 +36,6 @@ export default function FamilyGroupSettings(props) {
     console.log(members);
   }
 
-  let defaultValue = <DefaultInput>{familyName}</DefaultInput>;
-  if (toggleEdit) {
-    defaultValue = (
-      <InputField type="text" name="familyName" value={familyName}/>
-    );
-  }
-
   return (
     <Container>
       <CenterText>
@@ -54,21 +46,6 @@ export default function FamilyGroupSettings(props) {
             value={familyName}
             inputLabel="Family Name"
           />
-        </FormSection>
-        <FormSection>
-          <EditInput>
-            <div>
-              <InputLabel>
-                Family Name
-              </InputLabel>
-              {defaultValue}
-            </div>
-            {!toggleEdit ? (
-              <EditButton size="25px" onClick={() => setToggleEdit(!toggleEdit)}/>
-            ):
-              <CancelButton size="25px" onClick={() => setToggleEdit(!toggleEdit)}/>
-            }
-          </EditInput>
         </FormSection>
 
         <FormSection>
