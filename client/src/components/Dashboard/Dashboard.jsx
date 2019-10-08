d546ccc951c02506aeec3c8524e343870041b1e1
Gigi <gigiscleung@gmail.com>
2019-10-08 13:36:58 +1100

    working on Error component

--- client/src/components/Dashboard/Dashboard.jsx
+++ client/src/components/Dashboard/Dashboard.jsx
@@ -9,6 +9,7 @@ import {
 
 import { Container } from "ui/Helpers";
 import GET_CURRENT_USER from "queries/GetCurrentUser";
+import JollyError from "components/JollyError/JollyError";
 import JollyLoader from "components/JollyLoader/JollyLoader";
 import React from "react";
 import { useQuery } from "@apollo/react-hooks";
@@ -29,33 +30,34 @@ export default function Dashboard(props) {
   }
 
   if (loading) {
-    return <JollyLoader />;
+    return <JollyLoader/>;
   }
 
   return (
-    <Container>
-      <TextWrapper>
-        <h2>Hello {user.firstName}!</h2>
-        <p>Get started with one of the following actions: </p>
+    <JollyError/>
+    // <Container>
+    //   <TextWrapper>
+    //     <h2>Hello {user.firstName}!</h2>
+    //     <p>Get started with one of the following actions: </p>
 
-        <DashboardButtons onClick={() => props.history.push("/create-family")}>
-          <CreateFamily />
-          <ButtonHeading>
-            Create a Family
-            <span>And get the rest of your family on board.</span>
-          </ButtonHeading>
-          <GoToButton />
-        </DashboardButtons>
+    //     <DashboardButtons onClick={() => props.history.push("/create-family")}>
+    //       <CreateFamily />
+    //       <ButtonHeading>
+    //         Create a Family
+    //         <span>And get the rest of your family on board.</span>
+    //       </ButtonHeading>
+    //       <GoToButton />
+    //     </DashboardButtons>
 
-        <DashboardButtons>
-          <InviteFamily />
-          <ButtonHeading>
-            Join an existing Family
-            <span>Got an invite code? Join your family.</span>
-          </ButtonHeading>
-          <GoToButton />
-        </DashboardButtons>
-      </TextWrapper>
-    </Container>
+    //     <DashboardButtons>
+    //       <InviteFamily />
+    //       <ButtonHeading>
+    //         Join an existing Family
+    //         <span>Got an invite code? Join your family.</span>
+    //       </ButtonHeading>
+    //       <GoToButton />
+    //     </DashboardButtons>
+    //   </TextWrapper>
+    // </Container>
   );
 }
