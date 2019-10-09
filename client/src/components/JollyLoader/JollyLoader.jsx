352e2fb996e81d451cd19587979984ef40d9a80e
Rohyl <rohyljoshi@hotmail.com>
2019-10-09 23:59:09 +1100

    family quotes

--- client/src/components/JollyLoader/JollyLoader.jsx
+++ client/src/components/JollyLoader/JollyLoader.jsx
@@ -11,14 +11,17 @@ let usedQuotes = [];
 export default function JollyLoader(props) {
   let quotes = props.quotes || ["Loading..."];
   const [currentQuote, setQuote] = useState(quotes[0]);
-
+  useEffect(() => {
+    if (props.quotes)
+      setQuote(props.quotes[Math.floor(Math.random() * props.quotes.length)]);
+  }, [props.quotes]);
   useEffect(() => {
     const tick = () => {
       if (usedQuotes.length === quotes.length) usedQuotes = [];
       let randomQuote;
-      do {
-        randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
-      } while (usedQuotes.includes(randomQuote));
+      console.log(usedQuotes, quotes);
+      const unusedQuotes = quotes.filter(q => !usedQuotes.includes(q));
+      randomQuote = unusedQuotes[Math.floor(Math.random() * quotes.length)];
       usedQuotes.push(randomQuote);
       setQuote(randomQuote);
     };
