5b90dc93d6fe15f6e7df84f91c7d53f4b41333ea
Gigi <gigiscleung@gmail.com>
2019-10-18 15:45:38 +1100

    style intro hero

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,6 +1,13 @@
-import {ButtonSecondary} from "ui/Buttons";
+import {ButtonPrimary, ButtonSecondary} from "ui/Buttons";
+import {
+  FeaturesOverview,
+  Intro,
+  IntroContent,
+  PageHeader,
+  PitchActions
+} from './HomeStyles'
+
 import { Logo } from "ui/Logos";
-import {PageHeader} from './HomeStyles'
 import React from 'react'
 
 export default function Home() {
@@ -8,10 +15,25 @@ export default function Home() {
     <>
       <PageHeader>
         <Logo/>
-        <ButtonSecondary>
-          Login
-        </ButtonSecondary>
+        <ButtonSecondary>Login</ButtonSecondary>
       </PageHeader>
+      <Intro>
+        <IntroContent>
+          <h1>
+            Store your most significant memories in one place.
+          </h1>
+          <p>
+            Memento offers a safe and simple way to preserve your life events, milestones and artefacts for you and your family to appreciate for generations come.
+          </p>
+          <PitchActions>
+            <ButtonPrimary>Get Started</ButtonPrimary>
+            <ButtonSecondary>Learn More</ButtonSecondary>
+          </PitchActions>
+        </IntroContent>
+      </Intro>
+      <FeaturesOverview>
+        
+      </FeaturesOverview>
     </>
   )
 }
--- client/src/components/Home/HomeStyles.js
+++ client/src/components/Home/HomeStyles.js
@@ -7,3 +7,72 @@ export const PageHeader = styled.div`
   padding: 0 8vw;
   align-items: center;
 `;
+
+export const Intro = styled.section`
+  padding: 0 8vw;
+`;
+
+export const IntroContent = styled.div`
+  margin: 0 auto;
+  text-align: center;
+  padding: 70px 0 100px 0;
+  max-width: 480px;
+
+  h1 {
+    font-size: 24px;
+    margin-left: auto;
+    margin-right: auto;
+    max-width: 460px;
+  }
+
+  p {
+    opacity: 0.7;
+    line-height: 1.6em;
+    font-size: 15px;
+    margin-bottom: 50px;
+  }
+
+  @media screen and (min-width: ${props =>
+  props.theme.breakpoints.tabletPortrait}) {
+    max-width: 600px;
+
+    h1 {
+      max-width: 600px;
+    }
+
+    p {
+      font-size: 16px;
+    }
+  }
+`;
+
+export const PitchActions = styled.div`
+  text-align: center;
+
+  button {
+    margin: 0 10px;
+  }
+`;
+
+export const FeaturesOverview = styled.section`
+  margin: 0 auto;
+  padding: 0 8vw;
+  /* display: flex;
+  flex-direction: row;
+  flex-wrap: wrap; */
+`
+
+export const FeatureWrapper = styled.div`
+  padding: 10px;
+  display: flex;
+  flex-direction: row;
+`;
+
+export const FeatureImg = styled.div`
+  flex: 0 0 100px;
+
+  img {
+
+  }
+`;
+
