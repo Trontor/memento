57d9c05d4193690c3f3e62f706f685497e8a9e91
Rohyl <rohyljoshi@hotmail.com>
2019-10-18 20:26:05 +1100

    integrated: remove bookmark on bookmarks page

---
+++ client/src/queries/Bookmarks.js
@@ -0,0 +1,33 @@
+import gql from "graphql-tag";
+
+export const GET_BOOKMARKS = gql`
+  query bookmarks {
+    currentUser {
+      bookmarks {
+        createdAt
+        description
+        location
+        mementoId
+        tags
+        title
+        type
+        updatedAt
+        media {
+          url
+        }
+        dates {
+          year
+          month
+          day
+          dateId
+        }
+        uploader {
+          imageUrl
+        }
+        family {
+          name
+        }
+      }
+    }
+  }
+`;
