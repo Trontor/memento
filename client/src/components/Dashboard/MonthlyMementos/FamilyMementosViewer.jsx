94dc3a533f115061dafbccaeaf34458276b20650
Rohyl <rohyljoshi@hotmail.com>
2019-10-19 23:27:01 +1100

    MonthlyMemento subcomponents

---
+++ client/src/components/Dashboard/MonthlyMementos/FamilyMementosViewer.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import { useQuery } from "@apollo/react-hooks";
+import { GET_MEMENTOS } from "queries/Memento";
+import MementoCard from "components/MementoCard/MementoCard";
+
+const sameMonth = memento => {
+  const mementoDates = memento.dates;
+  const currentMonth = 2 || new Date().getMonth();
+  return mementoDates.some(date => date.month === currentMonth);
+};
+
+const sameYear = memento => {
+  const mementoDates = memento.dates;
+  const currentYear = new Date().getYear();
+  return mementoDates.some(date => date.year === currentYear);
+};
+
+export default function FamilyMementosViewer(props) {
+  const { familyId } = props;
+  const [mementos, setMementos] = useState(null);
+  // Load family mementos
+  useQuery(GET_MEMENTOS, {
+    variables: { id: familyId },
+    onCompleted: data => {
+      if (data && data.mementos) {
+        console.log(data);
+
+        setMementos(data.mementos);
+      }
+    },
+  });
+  if (!mementos) {
+    return null;
+  }
+  // Filter mementos by the same year
+  const filteredMementos = mementos.filter(sameMonth);
+  return (
+    <div>
+      {filteredMementos.map(memento => (
+        <MementoCard {...memento} />
+      ))}
+    </div>
+  );
+}
--- client/src/components/Dashboard/MonthlyMementos/MonthlyMementos.jsx
+++ client/src/components/Dashboard/MonthlyMementos/MonthlyMementos.jsx
@@ -1,5 +1,15 @@
 import React from "react";
+import { MonthlyMementosContainer } from "./MonthlyMementoStyles";
+import FamilyMementosViewer from "./FamilyMementosViewer";
 
-export default function MonthlyMementos() {
-  return <div>test</div>;
+export default function MonthlyMementos(props) {
+  const { familyIds } = props;
+  // Load
+  return (
+    <MonthlyMementosContainer>
+      {familyIds.map(id => (
+        <FamilyMementosViewer familyId={id} />
+      ))}
+    </MonthlyMementosContainer>
+  );
 }
