59c3f36ce37662584e9461301d8817cc464f6e5e
giggleinu <gigiscleung@gmail.com>
2019-10-25 03:13:01 +1100

    fix login and signup

--- client/src/ui/Helpers.jsx
+++ client/src/ui/Helpers.jsx
@@ -1,10 +1,16 @@
 import styled, { css } from "styled-components";
 
+export const PageWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: calc(100vh - 200px);
+  position: relative;
+`;
+
 export const FormWrapper = styled.div`
-  position: absolute;
-  top: 45%;
-  left: 50%;
-  transform: translate(-50%, -50%);
+  margin: auto;
   width: 70%;
 
   @media screen and (min-width: ${props =>
